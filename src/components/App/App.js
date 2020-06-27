2be574a9887bc3357d51d9fb5c9af48e27d5888d
almeidarenato <renatoam@ymail.com>
2020-06-27 19:50:25 -0300

    fix do PR #3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import '../../css/global.css';
-import './App.css';
+import "../../css/global.css";
+import "./App.css";
 
-import Header from '../Header/Header';
+import Header from "../Header/Header";
 import Home from "../Home/Home";
 import Cadastro from "../Cadastro/Cadastro";
 // import Footer from '../Footer/Footer';
@@ -13,13 +13,13 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Header/>
-          <Switch>
-              <Route exact path="/" component={Home}/>
-              <Route exact path="/cadastro " component={Cadastro}/>
-          </Switch>      
+        <Header />
+        <Switch>
+          <Route path="/cadastro" component={Cadastro} />
+          <Route exact path="/" component={Home} />
+        </Switch>
       </div>
-  </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
