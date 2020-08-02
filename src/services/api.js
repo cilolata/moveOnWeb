fbf348a1fc04a43cad7d234d3a4ba1f7f9c0f259
cilolata <cilolata@gmail.com>
2020-08-02 19:22:42 -0300

    change api

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'http://159.203.186.16',
-    baseURL: 'http://localhost:3333',
+    baseURL: 'http://159.203.186.16',
+    // baseURL: 'http://localhost:3333',
 });
 
 export default api;
